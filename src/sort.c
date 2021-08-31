142414efa28c426a7621c52d407fee0fcde95c1f
Louis RAFFIN <lraffin@e3r4p8.42.fr>
2021-08-31 18:00:38 +0200

    push_swap

--- src/sort.c
+++ src/sort.c
@@ -6,16 +6,23 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/31 17:17:28 by lraffin           #+#    #+#             */
-/*   Updated: 2021/08/31 17:27:17 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/08/31 18:00:21 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include "../include/push_swap.h"
 
-void	swap_a(t_board *stack)
+void	swap_a(t_board **stack)
 {
-	t_stack	*tmp;
+	t_stack	*first;
+	t_stack	*second;
+	t_stack	*third;
 
-	tmp = stack->b;
-	st
+	first = (*stack)->a;
+	second = (*stack)->a->next;
+	third = second->next;
+
+	first->next = third;
+	second->next = first;
+	(*stack)->a = second;
 }
