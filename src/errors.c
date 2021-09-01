318451fd3764bbb9ed7b2889c877f88834e4604f
louisnfr <louis.raffin@gmail.com>
2021-09-01 12:32:36 +0200

    push_swap

--- src/errors.c
+++ src/errors.c
@@ -6,7 +6,7 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/28 20:08:50 by lraffin           #+#    #+#             */
-/*   Updated: 2021/08/31 17:14:27 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/09/01 12:09:44 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,7 @@
 void	ft_terminate(char *error_message, t_board *stack)
 {
 	free(stack);
-	ft_putstr(error_message);
+	ft_putstr_fd(error_message, 2);
 	exit(EXIT_FAILURE);
 }
 
@@ -32,7 +32,8 @@ void	check_arg(char *string, t_board *stack)
 // doesnt check for errors
 int	parse_input(t_board *stack, char **av)
 {
-	int		i;
+	int	i;
+	int	arr[1000]
 
 	i = 0;
 	while (av[++i])
