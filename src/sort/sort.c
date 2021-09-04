d744f674df9c56669c825ccfabb43bbd3d636110
louisnfr <louis.raffin@gmail.com>
2021-09-04 20:18:09 +0200

    push_swap

--- src/sort/sort.c
+++ src/sort/sort.c
@@ -6,7 +6,7 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 20:29:59 by lraffin           #+#    #+#             */
-/*   Updated: 2021/09/04 17:07:19 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/09/04 20:17:40 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,11 +14,5 @@
 
 void	sort(t_board *stack)
 {
-	while (!is_empty(stack->a))
-		pb(stack);
-	while (!is_empty(stack->b))
-	{
-		bring_on_top_b(stack, largest(stack->b));
-		pa(stack);
-	}
+	(void)stack;
 }
