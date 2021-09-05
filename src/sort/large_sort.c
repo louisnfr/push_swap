551af5b23f712d214354e766c6c54471b64e4e0d
louisnfr <louis.raffin@gmail.com>
2021-09-06 00:08:16 +0200

    push_swap

--- src/sort/large_sort.c
+++ src/sort/large_sort.c
@@ -6,45 +6,27 @@
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/05 17:34:53 by lraffin           #+#    #+#             */
-/*   Updated: 2021/09/05 23:08:51 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/09/06 00:07:59 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/push_swap.h"
 
-/**
- * get array
- * sort array
- * get number of values
- * split in 5 chunks
- * find first number of chunk 1 each direction
- * get closest one on top
-**/
-
 void	large_sort(t_board *stack)
 {
-	// t_chunk	*chunks;
-	// int		*array;
-	// int		divide;
+	t_quart	*quart;
+	int		*array;
 
-	// chunks = malloc(sizeof(t_chunk));
-	// array = malloc(sizeof(int) * stack->length);
-	// if (!array || !chunks)
-	// {
-	// 	free(array);
-	// 	free(chunks);
-	// 	terminate(ERROR, stack);
-	// }
-	// get_array(stack->a, array);
-	// sort_array(array, stack->length);
-
-	while (!is_sorted(stack->a))
+	quart = malloc(sizeof(t_quart));
+	array = malloc(sizeof(int) * stack->length);
+	if (!array || !quart)
 	{
-		if (stack->a->value > stack->a->next->value)
-			sa(stack);
-		ra(stack);
+		free(quart);
+		free(array);
+		terminate(ERROR, stack);
 	}
-
-	// free(array);
-	// free(chunks);
+	get_array(stack->a, array);
+	sort_array(array, stack->length);
+	free(array);
+	free(quart);
 }
