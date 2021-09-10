5f2a184b85c51dd6f26254fedb7b38585f64dc77
louisnfr <louis.raffin@gmail.com>
2021-09-11 00:30:25 +0200

    push_swap

--- testers/tester.sh
+++ testers/tester.sh
@@ -1,143 +1,232 @@
+# **************************************************************************** #
+#                                                                              #
+#                                                         :::      ::::::::    #
+#    tester.sh                                          :+:      :+:    :+:    #
+#                                                     +:+ +:+         +:+      #
+#    By: cpereira <cpereira@student.42sp.org>       +#+  +:+       +#+         #
+#                                                 +#+#+#+#+#+   +#+            #
+#    Created: 2021/06/04 16:53:09 by lfrasson          #+#    #+#              #
+#    Updated: 2021/07/30 15:26:12 by pcunha           ###   ########.fr        #
+#                                                                              #
+# **************************************************************************** #
+
 #!/bin/bash
 
-# Created by lmalki-h with <3
-# Usage: ./push_swap_tester.sh [directory to push_swap and checker progs ] [stacksize 0R stacksize_min-stacksize_max ] [nb_of_test per stacksize]
-# Example 1: bash visualise.sh ../push_swap/ 5 100
-# Will test your program 100 times with a stack of 5 random ints
-# Example 2: ./push_swap_tester.sh ../push_swap/ 3-5 50
-# Will test your program 50 times with a stack of 3 random ints, then 50 times with 4 ints , then 50 times with 5 ints.
+NUM_TESTS=100
+PUSH_SWAP=../push_swap
+CKER=1
+RET_CKER="is not working"
 
-NOCOLOR='\033[0m'
-RED='\033[0;31m'
-GREEN='\033[0;32m'
-ORANGE='\033[0;33m'
-BLUE='\033[0;34m'
-PURPLE='\033[0;35m'
-CYAN='\033[0;36m'
-LIGHTGRAY='\033[0;37m'
-DARKGRAY='\033[1;30m'
-LIGHTRED='\033[1;31m'
-LIGHTGREEN='\033[1;32m'
-YELLOW='\033[1;33m'
-LIGHTBLUE='\033[1;34m'
-LIGHTPURPLE='\033[1;35m'
-LIGHTCYAN='\033[1;36m'
-WHITE='\033[1;37m'
+GREEN="\033[32m"
+YELLOW="\033[0;33m"
+RED="\033[0;31m"
+BLUE="\033[0;34m"
+MUTED="\033[1;30m"
+RESET="\033[0m"
+BOLD="\033[1m"
 
-if [[ $# -ne 3 ]]; then
-    printf "Usage: ./push_swap_tester.sh [directory-to-push_swap] [stacksize 0R range] [nb_of_test]\n" >&2
-    exit -1
+rm -rf log_error
+mkdir log_error
+mkdir log_error/five
+mkdir log_error/hundred
+mkdir log_error/five_hundred
+
+FLAG=all
+while getopts f:n: flag
+do
+	case "${flag}" in
+		f) FLAG=${OPTARG};;
+		n) NUM_TESTS=${OPTARG};;
+	esac
+done
+
+if ! command -v ruby &> /dev/null
+then
+	echo -e "${RED}${BOLD}Ruby not found. Aborting"
+	echo -e "If using Linux, try 'sudo apt-get install ruby' to install${RESET}"
+	exit
 fi
 
-rm -f push_swap_result.txt
-
-digit='^[0-9]+$' 		#digit number
-range='^[0-9]+-[0-9]+$' #range type 
-
-if [[ $2 =~ $range ]]; then
-	startRange=$(( ${2%-*} + 0))
-	endRange=$(( ${2##*-} + 0))
-elif [[ $2 =~ $digit ]]; then
-	startRange=$(( ${2%-*} + 0))
-	endRange=$(( ${2##*-} + 0))
+if [ "$(eval uname)" == "Linux" ]
+then
+	CHECKER=./checker_linux
 else
-	printf "${RED} error: %s must be a positive number or range\n${NOCOLOR}" "$2">&2
-	exit -1
+	CHECKER=./checker
 fi
 
-if ! [[ $3 =~ $digit ]]; then
-	printf "${RED}error: %s must be a positive number\n${NOCOLOR}" "$3" >&2
-	exit -1;
+if ! command -v $CHECKER &> /dev/null
+then
+	echo -en "${RED}${BOLD}\n./checker is not working.\nSo this script will only check the number of operations\n${RESET}"
+	CKER=0
 fi
 
-TotalNbTest=$(($3 + 0))
-
-if [[ $TotalNbTest -lt 1 ]]; then
-	printf "${RED}error: %s must be a positive number\n${NOCOLOR}" "$3" >&2
-	exit -1;
-fi 
-
-if (( $endRange < $startRange )); then
-	printf "${RED}error: invalid range\n${NOCOLOR}" >&2
-	exit -1
-fi
-
-if ! [[ -f "$1/push_swap" ]]; then
-	printf "${RED}error: could not find push_swap in $1 \n${NOCOLOR}" >&2
-	exit -1;
-elif ! [[ -f "$1/checker" ]]; then
-	printf "${RED}error: could not find checker in $1 \n${NOCOLOR}" >&2
-	exit -1;
-elif ! [[ -x "$1/push_swap" ]]; then
-	printf "${RED}error: cannot execute push_swap in $1 \n${NOCOLOR}" >&2
-	exit -1;
-elif ! [[ -x "$1/checker" ]]; then
-	printf "${RED}error: cannot execute checker in $1 \n${NOCOLOR}" >&2
-	exit -1;
+seq 1 5 | shuf > /dev/null
+if [ $? == 0 ]
+then
+	SHUF_OK=1
 else
-	printf "${GREEN}Testing push_swap with $TotalNbTest tests from $startRange to $endRange \n\n${NOCOLOR}" >&2
+	SHUF_OK=0
 fi
 
-for ((stack_size = $startRange; stack_size <= $endRange; stack_size++)); do
-	TOTAL=0
-	printf "${PURPLE} Generating random numbers for stack_size $stack_size...\n\n${NOCOLOR}"
-  for ((testNB = 0; testNB < $TotalNbTest; testNB++)); do
-  	printf "${DARKGRAY} TEST $testNB: ${NOCOLOR}"
-	ARG=`./genstack.pl $stack_size -1000 1000` ;
-	"./$1/push_swap" $ARG > push_swap_result.txt ;
-	RESULT_CHECKER=`"./$1/checker" $ARG < push_swap_result.txt`
-	if [[ "$RESULT_CHECKER" = "KO" ]]; then
-		printf "${RED}$RESULT_CHECKER ${NOCOLOR}"
+echo $SHUF_OK
+
+error()
+{
+	echo -en "${RESET}$1 => ${YELLOW} ./push_swap $2 => "
+	ARG="$2"
+	RET=`$PUSH_SWAP $ARG 2>&1`
+	if [ "$RET" != "Error" ];
+		then
+		echo -e "${RED}Fail${RESET}"
 	else
-		printf "${GREEN}$RESULT_CHECKER ${NOCOLOR}"
+		echo -e "${GREEN}Success${RESET}"
 	fi
-	MOVES=` cat push_swap_result.txt | wc -l`
-	if (( $stack_size <= 3 )) ; then
-		if (( $MOVES < 3 )); then
-			COLOR=${WHITE}
-		else
-			COLOR=${RED}
-		fi
-	elif (( $stack_size <= 5 )) ; then
-		if (( $MOVES < 8 )); then
-			COLOR=${WHITE}
-		elif (( $MOVES == 8 )); then
-			COLOR=${BLUE}
-		elif (( $MOVES < 13 )); then
-			COLOR=${GREEN}
-		else
-			COLOR=${RED}
-		fi
-	elif (( $stack_size <= 100 )) ; then
-		if (( $MOVES < 700 )); then
-			COLOR=${WHITE}
-		elif (( $MOVES < 900 )); then
-			COLOR=${BLUE}
-		elif (( $MOVES < 1100 )); then
-			COLOR=${GREEN}
-		elif (( $MOVES < 1500 )); then
-			COLOR=${ORANGE}
-		else
-			COLOR=${RED}
-		fi
-	elif (( $stack_size <= 500 )) ; then
-		if (( $MOVES < 5500 )); then
-			COLOR=${WHITE}
-		elif (( $MOVES < 7000 )); then
-			COLOR=${BLUE}
-		elif (( $MOVES < 8500 )); then
-			COLOR=${GREEN}
-		elif (( $MOVES < 11500 )); then
-			COLOR=${ORANGE}
-		else
-			COLOR=${RED}
-		fi
-	fi
-	printf "${COLOR} $MOVES ${NOCOLOR} instructions\n"
-	TOTAL=$(( $TOTAL + $MOVES ))
-  done
-  MEAN=$(( $TOTAL / $TotalNbTest ))
-  printf "\nMean: $MEAN for stack of size $stack_size \n\n"
-done 
+}
 
-rm -rf push_swap_result.txt
+do_noting()
+{
+	echo -en "${RESET}$1 => ${YELLOW} ./push_swap $2 => "
+	ARG="$2"
+	RET=`$PUSH_SWAP $ARG 2>&1`
+	if [ "$RET" != "" ];
+		then
+		echo -e "${RED}Fail${RESET}"
+	else
+		echo -e "${GREEN}Success${RESET}"
+	fi
+}
+
+checker3()
+{
+	echo -en "${RESET}$1 => ${YELLOW} ./push_swap $2 => "
+	ARG="$2"
+	if (( $CKER == 1))
+	then
+		RET_CKER=`$PUSH_SWAP $ARG | $CHECKER $ARG`
+	fi
+	RET2=`$PUSH_SWAP $ARG | wc -l | bc`
+	if [ "$RET_CKER" == "OK" ] && (($RET2 == 2)) || (($RET2 == 3));
+		then
+			echo -e "${GREEN}Success (checker $RET_CKER | $RET2 instructions)${RESET}"
+	else
+		echo -e "${RED}Fail (checker $RET_CKER | $RET2 instructions)${RESET}"
+	fi
+}
+
+checker5()
+{
+	echo -en "${RESET}$1 => ${YELLOW} ./push_swap $2 => "
+	ARG="$2"
+	if (( $CKER == 1))
+	then
+		RET_CKER=`$PUSH_SWAP $ARG | $CHECKER $ARG`
+	fi
+	RET2=`$PUSH_SWAP $ARG | wc -l | bc`
+	if [ "$RET_CKER" == "OK" ] && (($RET2 <= 12));
+		then
+			echo -e "${GREEN}Success (checker $RET_CKER | $RET2 instructions)${RESET}"
+	else
+		echo -e "${RED}Fail (checker $RET_CKER | $RET2 instructions)${RESET}"
+	fi
+}
+
+random_checker()
+{
+	echo -e "${RESET}$1"
+	if (($3 == 12));
+		then
+		FILE="log_error/five/test_"
+		fi
+	if (($3 == 1500));
+		then
+		FILE="log_error/hundred/test_"
+		fi
+	if (($3 == 11500));
+		then
+		FILE="log_error/five_hundred/test_"
+		fi
+	ERR=0
+	sum=0
+	count=0
+	for ((i = 0; i < NUM_TESTS; i++))
+		do
+			if [ $SHUF_OK == 1 ]
+			then
+				unset n
+				n=$4
+				ARG=$(seq 1 $n | awk '{print $1 - a}' a=$(expr $n / 2) | shuf | tr '\n' ' ')
+			else
+				ARG=`ruby -e "puts $2.to_a.shuffle.join(' ')"`
+			fi
+			if (( $CKER == 1))
+			then
+				RET_CKER=`$PUSH_SWAP $ARG | $CHECKER $ARG`
+			fi
+			RET2=`$PUSH_SWAP $ARG | wc -l | bc`
+			if [ "$RET_CKER" != "OK" ];
+				then
+				((ERR++))
+				echo -en "${RED}▓$RET2▓${RESET}"
+				echo "$1" > $FILE$i
+				echo "$ARG" >> $FILE$i
+				if (($3 == 12))
+					then
+					COMMANDS= $PUSH_SWAP $ARG >> $FILE$i
+					fi
+				echo "Qtt commands = $RET2" >> $FILE$i
+				echo "Test Checker = $RET_CKER" >> $FILE$i
+		else
+				echo -en "${GREEN}▓${RESET}"
+			fi
+
+			sum=$((sum + $RET2))
+			((count++))
+	done
+
+	sum=$((sum / $count))
+	if [ $ERR -eq 0 ] && [ $sum -le $3 ];
+		then
+		echo -en "${GREEN} Success"
+		echo -e " - Average $sum ${RESET}$"
+	else
+		echo -en "${RED} Fail $ERR / $count"
+		echo -e " - Average $sum ${RESET}$"
+	fi
+}
+
+echo $FLAG
+
+if [ "$FLAG" = "all" ] || [ "$FLAG" = "error" ]; then
+	echo -e "\n${BOLD}Error management${RESET}\n"
+	error "Non numeric parameters" "3 aa 1"
+	error "Duplicate numeric parameter" "3 3 1"
+	error "Only numeric parameters including one greater than MAX INT" "3 1 2147483648"
+	error "Only numeric parameters including one less than MIN INT" "3 1 -2147483649"
+fi
+
+if [ "$FLAG" = "all" ] || [ "$FLAG" = "sorted" ]; then
+	echo -e "\n${BOLD}Identity test${RESET}\n"
+	do_noting "Without any parameters"
+	do_noting "With a list that has already been sorted" "42"
+	do_noting "With a list that has already been sorted" "0 1 2 3"
+	do_noting "With a list that has already been sorted" "0 1 2 3 4 5 6 7 8 9"
+fi
+
+if [ "$FLAG" = "all" ] || [ "$FLAG" = "simple" ]; then
+	echo -e "\n${BOLD}Simple version${RESET}\n"
+	checker3 "Three numbers" "2 1 0"
+	checker5 "Five numbers" "1 5 2 4 3"
+	random_checker "Random list of Five numbers" "(0..4)" 12 5
+fi
+
+if [ "$FLAG" = "all" ] || [ "$FLAG" = "middle" ]; then
+	echo -e "\n${BOLD}Middle version${RESET}\n"
+	echo -e "less than 700\t-> 5\nless than 900\t-> 4\nless than 1100\t-> 3\nless than 1300\t-> 2\nless than 1500\t-> 1\n"
+	random_checker "Random list of hundred numbers -50 to 49" "(-50..49)" 1500 100
+fi
+
+if [ "$FLAG" = "all" ] || [ "$FLAG" = "advanced" ]; then
+	echo -e "\n${BOLD}Advanced version${RESET}\n"
+	echo -e "less than 5500\t-> 5\nless than 7000\t-> 4\nless than 8500\t-> 3\nless than 10000\t-> 2\nless than 11500\t-> 1\n"
+	random_checker "Random list of five hundred numbers 0 to 499" "(0..499)" 11500 500
+fi
