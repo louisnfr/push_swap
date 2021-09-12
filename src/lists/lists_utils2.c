a504d7f52d8189e83ca251e99558fea1a8d72f54
louisnfr <louis.raffin@gmail.com>
2021-09-13 00:11:48 +0200

    push_swap

--- src/lists/lists_utils2.c
+++ src/lists/lists_utils2.c
@@ -1,12 +1,12 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   utils2.c                                           :+:      :+:    :+:   */
+/*   lists_utils2.c                                     :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: lraffin <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 19:38:20 by lraffin           #+#    #+#             */
-/*   Updated: 2021/09/07 02:11:16 by lraffin          ###   ########.fr       */
+/*   Updated: 2021/09/13 00:08:49 by lraffin          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,24 +29,6 @@ void	bring_on_top_a(t_board *stack, int x)
 	else
 		while (!is_on_top(stack->a, x))
 			rra(stack, 1);
-}
-
-void	bring_on_top_b(t_board *stack, int x)
-{
-	if (!stack || is_on_top(stack->b, x))
-		return ;
-	if (where_is(x, stack->b) == 1)
-		while (!is_on_top(stack->b, x))
-			rb(stack, 1);
-	else
-		while (!is_on_top(stack->b, x))
-			rrb(stack, 1);
-}
-
-void	bring_b_push_a(t_board *stack, int x)
-{
-	bring_on_top_b(stack, x);
-	pa(stack, 1);
 }
 
 void	bring_a_push_b(t_board *stack, int x)
